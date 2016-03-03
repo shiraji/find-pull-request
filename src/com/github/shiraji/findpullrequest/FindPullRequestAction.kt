44ef0ce2003113258530ee020a837f5b57cdef07
shiraji <isogai.shiraji@gmail.com>
2016-03-03 16:19:06 +0900

    Make it Kotlin

---
+++ src/com/github/shiraji/findpullrequest/FindPullRequestAction.kt
@@ -0,0 +1,10 @@
+package com.github.shiraji.findpullrequest
+
+import com.intellij.openapi.actionSystem.AnAction
+import com.intellij.openapi.actionSystem.AnActionEvent
+
+class FindPullRequestAction : AnAction() {
+
+    override fun actionPerformed(e: AnActionEvent) {
+    }
+}
