d9bb4022927f5cc97743922c4002c4d2f1419607
shiraji <isogai.shiraji@gmail.com>
2016-03-04 14:59:20 +0900

    Try other way to get commit

--- src/com/github/shiraji/findpullrequest/FindPullRequestAction.kt
+++ src/com/github/shiraji/findpullrequest/FindPullRequestAction.kt
@@ -6,17 +6,36 @@ import com.intellij.notification.Notifications
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.actionSystem.CommonDataKeys
-import com.intellij.util.containers.ContainerUtil
-import com.intellij.vcs.log.VcsLogDataKeys
+import com.intellij.openapi.fileEditor.FileDocumentManager
+import com.intellij.openapi.project.Project
+import git4idea.repo.GitRepository
+import org.jetbrains.plugins.github.util.GithubUtil
 
 class FindPullRequestAction : AnAction() {
 
     override fun actionPerformed(e: AnActionEvent) {
-        val project = e.getRequiredData(CommonDataKeys.PROJECT)
-        val commit = ContainerUtil.getFirstItem(e.getRequiredData(VcsLogDataKeys.VCS_LOG).selectedCommits)
-
+        val eventData = calcData(e)
         Notifications.Bus.notify(Notification("Plugin Importer+Exporter",
                 "Plugin Importer+Exporter",
-                "Commit: " + commit, NotificationType.INFORMATION));
+                "EventData: " + eventData.toString(), NotificationType.INFORMATION))
+    }
+
+    private fun calcData(e : AnActionEvent): EventData? {
+        val project = e.getData(CommonDataKeys.PROJECT)
+        project ?: return null
+
+        val virtualFile = e.getData(CommonDataKeys.VIRTUAL_FILE)
+        virtualFile ?: return null
+
+        val document = FileDocumentManager.getInstance().getDocument(virtualFile)
+        document ?: return null
+
+        val repository = GithubUtil.getGitRepository(project, virtualFile)
+        repository ?: return null
+
+        return EventData(project, repository)
+    }
+
+    private data class EventData(val myProject: Project, val myRepository: GitRepository) {
     }
 }
