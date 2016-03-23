4f74a695fd21d56223f60da73f053838e06519ef
磯貝 <isogai.shiraji@gmail.com>
2016-03-23 17:39:02 +0900

    List merged commits from pull request hash

--- src/com/github/shiraji/findpullrequest/action/FindPullRequestAction.kt
+++ src/com/github/shiraji/findpullrequest/action/FindPullRequestAction.kt
@@ -1,26 +1,11 @@
 package com.github.shiraji.findpullrequest.action
 
-import com.intellij.notification.Notification
-import com.intellij.notification.NotificationType
-import com.intellij.notification.Notifications
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.actionSystem.CommonDataKeys
-import com.intellij.openapi.fileEditor.FileDocumentManager
-import com.intellij.openapi.progress.ProcessCanceledException
-import com.intellij.openapi.progress.ProgressIndicator
-import com.intellij.openapi.progress.ProgressManager
-import com.intellij.openapi.progress.Task
-import com.intellij.openapi.project.Project
-import com.intellij.openapi.util.Ref
-import com.intellij.openapi.vcs.VcsException
-import com.intellij.openapi.vfs.VirtualFile
-import com.intellij.vcsUtil.VcsUtil
-import git4idea.GitRevisionNumber
-import git4idea.GitVcs
 import git4idea.annotate.GitFileAnnotation
+import git4idea.changes.GitChangeUtils
 import git4idea.history.GitHistoryUtils
-import git4idea.repo.GitRepository
 import org.jetbrains.plugins.github.util.GithubUtil
 
 class FindPullRequestAction : AnAction() {
@@ -34,26 +19,17 @@ class FindPullRequestAction : AnAction() {
         }
         val repository = GithubUtil.getGitRepository(project, virtualFile) ?: return
         val annotate = repository.vcs?.annotationProvider?.annotate(virtualFile) as GitFileAnnotation? ?: return
-        val lineNumber = editor?.document?.getLineNumber(editor.selectionModel.selectionStart)?.plus(1) ?: return
-        val revisionHash = annotate.originalRevision(lineNumber)
-        val revisions = annotate.revisions ?: return
+        val lineNumber = editor?.document?.getLineNumber(editor.selectionModel.selectionStart) ?: return
+        val revisionHash = annotate.originalRevision(lineNumber) ?: return
+        val histories = GitHistoryUtils.history(project, repository.root, "$revisionHash..master", "--grep=Merge pull request", "--merges", "--ancestry-path", "--reverse")
+        val pullRequestCommit = histories.first()
+        val mergedCommits = GitHistoryUtils.history(project, repository.root, "${pullRequestCommit.id}^..${pullRequestCommit.id}")
 
-        val pullRequestRev = revisions.subList(0, revisions.indexOfFirst { it.revisionNumber == revisionHash }).findLast {
-            val commitMessage = it.commitMessage
-            commitMessage != null && commitMessage.indexOf("Merge pull request #") > 0
+        if(mergedCommits.filter { it.id.asString() == revisionHash.asString() }.size == 0) {
+            System.out.println("No hash!!! $revisionHash")
+        }else {
+            System.out.println("Found!!! $revisionHash")
         }
 
-        // TODO: do this http://joey.aghion.com/find-the-github-pull-request-for-a-commit/
-        val histories = GitHistoryUtils.history(project, repository.root, "$revisionHash..master", "-100")
-        val pullRequestCommit = histories.findLast {
-            it.fullMessage.indexOf("Merge pull request #") >= 0
-        }
-
-        System.out.println(pullRequestCommit)
-
-        Notifications.Bus.notify(Notification("Plugin Importer+Exporter",
-                "Plugin Importer+Exporter",
-                "hash: $revisionHash currentRev: ${annotate?.currentRevision} pullRequestRev: $pullRequestRev",
-                NotificationType.INFORMATION))
     }
 }
