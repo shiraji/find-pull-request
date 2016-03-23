03c708e9a85a03dd7d3d03db2af97a9ea8718f48
shiraji <isogai.shiraji@gmail.com>
2016-03-24 02:37:09 +0900

    Refactor a lot

--- src/com/github/shiraji/findpullrequest/action/FindPullRequestAction.kt
+++ src/com/github/shiraji/findpullrequest/action/FindPullRequestAction.kt
@@ -1,51 +1,38 @@
 package com.github.shiraji.findpullrequest.action
 
 import com.github.shiraji.findpullrequest.model.FindPullRequestModel
-import com.github.shiraji.getPullRequestNumber
-import com.github.shiraji.subtract
-import com.github.shiraji.toMd5
 import com.intellij.ide.BrowserUtil
+import com.intellij.notification.Notification
+import com.intellij.notification.NotificationType
+import com.intellij.notification.Notifications
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
-import com.intellij.openapi.actionSystem.CommonDataKeys
-import git4idea.GitFileRevision
-import git4idea.history.GitHistoryUtils
-import org.jetbrains.plugins.github.util.GithubUrlUtil
-import org.jetbrains.plugins.github.util.GithubUtil
 
 class FindPullRequestAction : AnAction() {
 
     override fun actionPerformed(e: AnActionEvent) {
         val model = FindPullRequestModel(e)
-        if (!model.isValid()) return
+        if (!model.isEnable()) return
 
-        val project = e.getData(CommonDataKeys.PROJECT);
-        val virtualFile = e.getData(CommonDataKeys.VIRTUAL_FILE);
-        val editor = e.getData(CommonDataKeys.EDITOR);
-        if (virtualFile == null || project == null || project.isDisposed || editor == null) {
-            return;
+        val url = model.findPullRequestUrlOrCommitUrl() ?: return
+
+        if (!model.isPullRequestUrl(url)) {
+            Notifications.Bus.notify(Notification("FindPullRequest",
+                    "Find Pull Request",
+                    "Could not find the pull request. Open the commit which the line is added",
+                    NotificationType.INFORMATION))
         }
 
-        val repository = GithubUtil.getGitRepository(project, virtualFile) ?: return
-        val annotate = repository.vcs?.annotationProvider?.annotate(virtualFile) ?: return
-        val lineNumber = editor.document.getLineNumber(editor.selectionModel.selectionStart)
-        val revisionHash = annotate.originalRevision(lineNumber) ?: return
-        val histories = GitHistoryUtils.history(project, repository.root, "$revisionHash..master", "--grep=Merge pull request", "--merges", "--ancestry-path", "--reverse")
-        val pullRequestCommit = histories.first()
-        val mergedCommits = GitHistoryUtils.history(project, repository.root, "${pullRequestCommit.id}^..${pullRequestCommit.id}")
-        val pullRequestUrl = GithubUtil.findUpstreamRemote(repository) ?: GithubUtil.findGithubRemoteUrl(repository) ?: return
-        val revision = annotate.revisions?.single { it.revisionNumber == revisionHash } as GitFileRevision
-        val md5 = revision.path.path.subtract("${repository.gitDir.parent.presentableUrl.toString()}/").toMd5()
-        var url: String?
-        if(mergedCommits.filter { it.id.asString() == revisionHash.asString() }.size == 0) {
-            val originUrl = repository.remotes.singleOrNull { it.name == "origin" }?.firstUrl ?: return
-            val githubUrl = GithubUrlUtil.makeGithubRepoUrlFromRemoteUrl(originUrl, "https://" + GithubUrlUtil.getHostFromUrl(originUrl))
-            url = "$githubUrl/commit/$revisionHash#diff-$md5"
-        }else {
-            val pullRequestNumber = pullRequestCommit.getPullRequestNumber()
-            val githubUrl = GithubUrlUtil.makeGithubRepoUrlFromRemoteUrl(pullRequestUrl, "https://" + GithubUrlUtil.getHostFromUrl(pullRequestUrl))
-            url = "$githubUrl/pull/$pullRequestNumber/files#diff-$md5"
-        }
         BrowserUtil.open(url)
+    }
+
+    override fun update(e: AnActionEvent?) {
+        e ?: return
+        super.update(e)
+
+        if(!FindPullRequestModel(e).isEnable()) {
+            e.presentation.isEnabled = false
+            e.presentation.isVisible = false
+        }
     }
 }
--- src/com/github/shiraji/findpullrequest/model/FindPullRequestModel.kt
+++ src/com/github/shiraji/findpullrequest/model/FindPullRequestModel.kt
@@ -1,14 +1,25 @@
 package com.github.shiraji.findpullrequest.model
 
+import com.github.shiraji.getPullRequestNumber
+import com.github.shiraji.subtract
+import com.github.shiraji.toMd5
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.actionSystem.CommonDataKeys
 import com.intellij.openapi.editor.Editor
 import com.intellij.openapi.project.Project
+import com.intellij.openapi.vcs.annotate.FileAnnotation
+import com.intellij.openapi.vcs.history.VcsRevisionNumber
 import com.intellij.openapi.vfs.VirtualFile
+import git4idea.GitCommit
+import git4idea.GitFileRevision
+import git4idea.history.GitHistoryUtils
+import git4idea.repo.GitRepository
+import org.jetbrains.plugins.github.util.GithubUrlUtil
+import org.jetbrains.plugins.github.util.GithubUtil
 
 class FindPullRequestModel {
-    val project : Project?
-    val editor : Editor?
+    val project: Project?
+    val editor: Editor?
     val virtualFile: VirtualFile?
 
     constructor(e: AnActionEvent) {
@@ -17,10 +28,59 @@ class FindPullRequestModel {
         virtualFile = e.getData(CommonDataKeys.VIRTUAL_FILE)
     }
 
-    fun isValid(): Boolean {
-        return project != null
-                && !project.isDisposed
-                && editor != null
-                && virtualFile != null
+    fun isEnable(): Boolean {
+        if (project == null || project.isDisposed || editor == null || virtualFile == null) {
+            return false
+        }
+        val startLine = editor.document.getLineNumber(editor.selectionModel.selectionStart)
+        val endLine = editor.document.getLineNumber(editor.selectionModel.selectionEnd)
+        return startLine == endLine
+    }
+
+    fun findPullRequestUrlOrCommitUrl(): String? {
+        if (project == null || editor == null || virtualFile == null) {
+            return null
+        }
+        val repository = GithubUtil.getGitRepository(project, virtualFile) ?: return null
+        val annotate = repository.vcs?.annotationProvider?.annotate(virtualFile) ?: return null
+        val lineNumber = editor.document.getLineNumber(editor.selectionModel.selectionStart)
+        val revisionHash = annotate.originalRevision(lineNumber) ?: return null
+        val githubRepoUrl = createGithubRepoUrl(repository) ?: return null
+        val pullRequestCommit = findPullRequestCommit(repository, revisionHash)
+        var path = if (pullRequestCommit == null) {
+            "commit/$revisionHash"
+        } else {
+            "pull/${pullRequestCommit.getPullRequestNumber()}/files"
+        }
+        return "$githubRepoUrl/$path#diff-${createMd5Hash(repository, annotate, revisionHash)}"
+    }
+
+    fun isPullRequestUrl(url: String): Boolean {
+        return url.contains("pull/")
+    }
+
+    private fun createGithubRepoUrl(repository: GitRepository): String? {
+        val remoteUrl: String = GithubUtil.findUpstreamRemote(repository) ?: GithubUtil.findGithubRemoteUrl(repository) ?: return null
+        return GithubUrlUtil.makeGithubRepoUrlFromRemoteUrl(remoteUrl, "https://" + GithubUrlUtil.getHostFromUrl(remoteUrl))
+    }
+
+    private fun createMd5Hash(repository: GitRepository, annotate: FileAnnotation, revisionHash: VcsRevisionNumber): String {
+        val revision = annotate.revisions?.single { it.revisionNumber == revisionHash } as GitFileRevision
+        return revision.path.path.subtract("${repository.gitDir.parent.presentableUrl.toString()}/").toMd5()
+    }
+
+    private fun findPullRequestCommit(repository: GitRepository, revisionHash: VcsRevisionNumber): GitCommit? {
+        if(project == null) return null
+        // pull request commit can be found `git log hash..master --grep="Merge pull request" --merges --ancestry-path --reverse`
+        val pullRequestCommit = GitHistoryUtils.history(project, repository.root, "$revisionHash..master", "--grep=Merge pull request", "--merges", "--ancestry-path", "--reverse").firstOrNull() ?: return null
+
+        // List all commits that are merged when pull request commits are merged. The command is `git log hash^..hash`
+        val mergedCommits = GitHistoryUtils.history(project, repository.root, "${pullRequestCommit.id}^..${pullRequestCommit.id}")
+
+        return if (mergedCommits.filter { it.id.asString() == revisionHash.asString() }.size == 0) {
+            null
+        } else {
+            pullRequestCommit
+        }
     }
 }
