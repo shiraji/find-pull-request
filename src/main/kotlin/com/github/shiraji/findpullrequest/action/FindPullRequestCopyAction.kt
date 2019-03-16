1956d5e0e612a6bdfae26b342c3dbbdbae495389
shiraji <isogai.shiraji@gmail.com>
2019-03-16 21:39:02 +0900

    Create copy Pull Request action

--- src/main/kotlin/com/github/shiraji/findpullrequest/action/FindPullRequestCopyAction.kt
+++ src/main/kotlin/com/github/shiraji/findpullrequest/action/FindPullRequestCopyAction.kt
@@ -6,23 +6,27 @@ import com.github.shiraji.findpullrequest.helper.showInfoNotification
 import com.github.shiraji.findpullrequest.model.FindPullRequestModel
 import com.github.shiraji.findpullrequest.model.isDebugMode
 import com.github.shiraji.findpullrequest.model.isJumpToFile
+import com.intellij.icons.AllIcons
 import com.intellij.ide.BrowserUtil
 import com.intellij.ide.util.PropertiesComponent
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.actionSystem.CommonDataKeys
 import com.intellij.openapi.editor.Editor
+import com.intellij.openapi.ide.CopyPasteManager
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.vcs.VcsException
 import com.intellij.openapi.vfs.VirtualFile
+import com.intellij.util.ui.TextTransferable
 import git4idea.GitUtil
 import git4idea.repo.GitRepository
 import java.net.URLEncoder
 
-class FindPullRequestAction : BaseFindPullRequestAction() {
+class FindPullRequestCopyAction : BaseFindPullRequestAction() {
 
     override fun actionPerform(url: String) {
-        BrowserUtil.open(url)
+        CopyPasteManager.getInstance().setContents(TextTransferable(url))
+        showInfoNotification("Copied!")
     }
 
 }
