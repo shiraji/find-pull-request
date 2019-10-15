4a883362ad06138ccacab8c8b831f30d787fccc3
shiraji <isogai.shiraji@gmail.com>
2019-10-16 01:50:05 +0900

    Replace GitHistoryUtils.history with collectTimedCommits
    history method is really slow when the repo contains a lots of commits
    if the method parameters does not specify the number of commit history
    this plugin wants. Also, most of places that use history method does
    not require all commit info. Therefore, I shifted to use
    collectTimedCommits. And then, if it requires other commit info, use
    history with the parameter that limit the number of commit.

--- src/main/kotlin/com/github/shiraji/findpullrequest/model/GitHistoryService.kt
+++ src/main/kotlin/com/github/shiraji/findpullrequest/model/GitHistoryService.kt
@@ -3,6 +3,7 @@ package com.github.shiraji.findpullrequest.model
 import com.github.shiraji.getNumberFromCommitMessage
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.vcs.history.VcsRevisionNumber
+import com.intellij.openapi.vfs.LocalFileSystem
 import git4idea.GitCommit
 import git4idea.history.GitHistoryUtils
 import git4idea.repo.GitRepository
@@ -10,26 +11,34 @@ import git4idea.repo.GitRepository
 class GitHistoryService {
 
     fun findLatestMergeCommit(project: Project): GitCommit? {
-        val results = GitHistoryUtils.history(project, project.baseDir, "--merges")
-        return results.firstOrNull()
+        val basePath = project.basePath ?: return null
+        val root = LocalFileSystem.getInstance().findFileByPath(basePath) ?: return null
+        val commit = GitHistoryUtils.collectTimedCommits(project, root, "--merges", "-1").firstOrNull() ?: return null
+        return GitHistoryUtils.history(project, root, commit.id.asString(), "-1").firstOrNull()
     }
 
     fun findCommitLog(project: Project, repository: GitRepository, revisionHash: VcsRevisionNumber): GitCommit {
-        return GitHistoryUtils.history(project, repository.root, "$revisionHash").first()
+        return GitHistoryUtils.history(project, repository.root, "$revisionHash", "-1").first()
     }
 
     fun findMergedCommit(project: Project, repository: GitRepository, revisionHash: VcsRevisionNumber): GitCommit? {
         // See https://stackoverflow.com/questions/8475448/find-merge-commit-which-include-a-specific-commit
-        //
-        // I think there is a bug in history() since it does not keep the order correctly
-        // It seems GitLogUtil#readFullDetails is the place that store the results in list
-        val ancestryPathCommits =
-            GitHistoryUtils.history(project, repository.root, "$revisionHash..HEAD", "--merges", "--ancestry-path")
-                .sortedBy { it.commitTime }
-        val firstParentsCommits =
-            GitHistoryUtils.history(project, repository.root, "$revisionHash..HEAD", "--merges", "--first-parent")
-                .sortedBy { it.commitTime }
-        return ancestryPathCommits.firstOrNull { firstParentsCommits.contains(it) }
+        val ancestryPathCommits = GitHistoryUtils.collectTimedCommits(
+            project,
+            repository.root,
+            "$revisionHash..HEAD",
+            "--merges",
+            "--ancestry-path"
+        )
+        val firstParentsCommits = GitHistoryUtils.collectTimedCommits(
+            project,
+            repository.root,
+            "$revisionHash..HEAD",
+            "--merges",
+            "--first-parent"
+        )
+        val commit = ancestryPathCommits.lastOrNull { firstParentsCommits.contains(it) } ?: return null
+        return GitHistoryUtils.history(project, repository.root, commit.id.asString(), "-1").firstOrNull()
     }
 
     fun listCommitsFromMergedCommit(project: Project, repository: GitRepository, pullRequestCommit: GitCommit): List<GitCommit> {
